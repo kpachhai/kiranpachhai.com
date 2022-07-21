1518aff79ebefc35e92b0d98e49a610bfcd6b111
Kiran Pachhai <kiran.pachhai@gmail.com>
2022-07-21 11:12:12 -0400

    First commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,97 @@
+import './index.scss'
+import LogoTitle from '../../assets/images/logo-kp.png'
+import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
+import Loader from 'react-loaders'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const blockchainEngineerArray = [
+    'B',
+    'l',
+    'o',
+    'c',
+    'k',
+    'c',
+    'h',
+    'a',
+    'i',
+    'n',
+    ' ',
+    'e',
+    'n',
+    'g',
+    'i',
+    'n',
+    'e',
+    'e',
+    'r',
+  ]
+  const webDeveloperArray = [
+    'W',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <span> </span>
+            <span className={`${letterClass} _14`}>a</span>
+            <span> </span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={blockchainEngineerArray}
+              idx={15}
+            />
+            <br />
+            <span className={`${letterClass} _15`}>and a</span>
+            <span> </span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={webDeveloperArray}
+              idx={22}
+            />
+          </h1>
+          <h2>
+            Fullstack Developer / Devops Engineer / Python & Golang Expert
+          </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Home
